36ffb715d032b6f3e65e5c4ff17b0f5335d66d34
Jesus Arriola <piracarter@hotmail.com>
2018-06-20 20:26:18 -0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -7,9 +7,9 @@ function Connection() {
     this.pool = mysql.createPool({
       connectionLimit: 10,
       host: 'tododb',
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DATABASE
     });
   };
 
